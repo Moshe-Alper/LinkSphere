e9912541b8843255086e146c9f20b122ab1f6cc9
Moshik Alper <moshikalper@gmail.com>
2025-04-28 15:43:14 +0300

    Implement login functionality with Firebase authentication

--- src/cmps/LoginComponent.jsx
+++ src/cmps/LoginComponent.jsx
@@ -1,9 +1,33 @@
 import React from 'react';
+import '../Sass/LoginComponent.scss'
+import { LoginApi } from '../api/AuthApi';
 
 export default function LoginComponent() {
+
+    const login = () => {
+        let res = LoginApi()
+        console.log(res)
+    }
     return (
-        <div>
+        <div className="login-wrapper">
             <h1>LoginComponent</h1>
+            <div className="auth-inputs">
+                <input
+                    onChange={(e) =>
+                        setCredentials((prev) => ({ ...prev, email: e.target.value }))
+                    }
+                    className="common-input"
+                    placeholder="Enter your Email"
+                />
+                <input
+                    onChange={(e) =>
+                        setCredentials((prev) => ({ ...prev, password: e.target.value }))
+                    }
+                    className="common-input"
+                    placeholder="Enter your Password"
+                />
+            </div>
+            <button onClick={login} className="login-btn">Log in to LinkSphere</button>
         </div>
     )
 }
