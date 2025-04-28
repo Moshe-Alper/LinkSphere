2528a2adde22706608c722957f4d88b6a9153795
Moshik Alper <moshikalper@gmail.com>
2025-04-28 14:48:58 +0300

    Init repo

---
+++ src/firebaseConfig.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDb6jQWvXv1R6HNHrGtFKQpr_1HMXwqoxU",
+  authDomain: "linksphere-14365.firebaseapp.com",
+  projectId: "linksphere-14365",
+  storageBucket: "linksphere-14365.firebasestorage.app",
+  messagingSenderId: "354824921058",
+  appId: "1:354824921058:web:33e1bef1c93200c8eaa3bb"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
