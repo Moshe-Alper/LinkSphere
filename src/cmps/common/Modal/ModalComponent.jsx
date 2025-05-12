08c5b78d968b17ca60e0d707b10f0cefaef26bd8
Moshik Alper <moshikalper@gmail.com>
2025-05-12 16:02:57 +0300

    Add model component when posting update on home

---
+++ src/cmps/common/Modal/ModalComponent.jsx
@@ -0,0 +1,36 @@
+import React from 'react';
+import { Button, Modal } from 'antd';
+import "./ModalComponent.scss"
+
+const ModalComponent = ({ modalOpen, setModalOpen, setStatus, status }) => {
+
+
+  return (
+    <>
+
+      <Modal
+        title="Create a Post"
+        centered
+        open={modalOpen}
+        onOk={() => setModalOpen(false)}
+        onCancel={() => setModalOpen(false)}
+        footer={[
+          <Button key="submit" type="primary" disabled={status.length > 0 ? false : true}>
+            Post
+          </Button>
+        ]}
+
+      >
+        <input
+          className="modal-input"
+          type="text"
+          placeholder="What do you want to talk about" 
+          onChange={(event) => setStatus(event.target.value)}
+          value={status}
+          />
+      </Modal>
+    </>
+  );
+};
+
+export default ModalComponent
--- src/cmps/common/PostUpdate/PostUpdate.jsx
+++ src/cmps/common/PostUpdate/PostUpdate.jsx
@@ -1,11 +1,21 @@
+import { useState } from "react"
 import "./PostUpdate.css"
+import ModalComponent from "../Modal/ModalComponent"
 
 export function PostStatus() {
+    const [modalOpen, setModalOpen] = useState(false)
+    const [status, setStatus] = useState('')
     return (
         <section className="post-status-main">
             <div className="post-status">
-            <button className="open-post-modal-btn">Start a Post</button>
+                <button className="open-post-modal-btn" onClick={() => setModalOpen(true)}>Start a Post</button>
             </div>
+            <ModalComponent 
+            modalOpen={modalOpen} 
+            setModalOpen={setModalOpen} 
+            status= {status}
+            setStatus={setStatus}
+            />
         </section>
     )
 }
