c42bd35774b64171a1b4d7fea3d235019cb9c502
Moshik Alper <moshikalper@gmail.com>
2025-06-23 16:51:05 +0300

    Update index title to 'LinkSphere', enhance PostsCard layout with action icons and improved styling for user details

--- src/cmps/common/PostsCard/PostsCard.jsx
+++ src/cmps/common/PostsCard/PostsCard.jsx
@@ -3,6 +3,7 @@ import './PostsCard.scss'
 import { useNavigate } from 'react-router-dom'
 import { getCurrentUser, getAllUsers } from '../../../api/FirestoreAPI'
 import { useMemo, useState } from 'react'
+import { BsPencil, BsTrash } from "react-icons/bs"
 
 export function PostsCard({ posts }) {
     let navigate = useNavigate()
@@ -18,28 +19,34 @@ export function PostsCard({ posts }) {
         .filter((item) => item.id === posts.userID)
         .map((item) => item.imageLink)[0];
 
-return (
-    <section className="posts-card">
-        <div className="post-header">
-            {userImageLink && (
-                <img src={userImageLink} alt="profile-image" className="profile-image" />
-            )}
-            <div className="user-info">
-                <p 
-                    className="name"
-                    onClick={() => navigate("/profile",
-                    { state: { id: posts?.userID, email: posts.userEmail } })}>
-                    {posts.userName}
-                </p>
-                <p className="timestamp">{posts.timestamp}</p>
-            </div>
-        </div>
-        <p className="status">{posts.status}</p>
-        <LikeButton 
-            userId={currentUser?.userID}
-            postId={posts.id}
-            currentUser={currentUser}
-        />
-    </section>
-)
+ return (
+        <section className="posts-card">
+            <header className="post-header">
+                <div className="user-info">
+                    {userImageLink && (
+                        <img src={userImageLink} alt="profile-image" className="profile-image" />
+                    )}
+                    <div className="user-details">
+                        <p 
+                            className="name"
+                            onClick={() => navigate("/profile",
+                            { state: { id: posts?.userID, email: posts.userEmail } })}>
+                            {posts.userName}
+                        </p>
+                        <p className="timestamp">{posts.timestamp}</p>
+                    </div>
+                </div>
+                <div className="action-container">
+                    <BsPencil size={30} className="action-icon" />
+                    <BsTrash size={30} className="action-icon" />
+                </div>
+            </header>
+            <p className="status">{posts.status}</p>
+            <LikeButton 
+                userId={currentUser?.userID}
+                postId={posts.id}
+                currentUser={currentUser}
+            />
+        </section>
+    )
 }
